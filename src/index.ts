4a0d347b07b84b66a4e5e6e5ada854f4898df3ca
Phumrapee Limpianchop <git@rayriffy.com>
2024-04-28 20:48:46 +0900

    fix: redirect

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,12 @@ import { cors } from "@elysiajs/cors";
 
 import { router as appleMaps } from "./packlets/appleMaps/router";
 
-const app = new Elysia().use(swagger()).use(cors()).use(appleMaps).listen(3000);
+const app = new Elysia()
+  .use(swagger())
+  .use(cors())
+  .get("/", ({ set }) => (set.redirect = "/swagger"))
+  .use(appleMaps)
+  .listen(3000);
 
 console.log(
   `🦊 Swagger is running at https://${app.server?.hostname}:${app.server?.port}/swagger`,
