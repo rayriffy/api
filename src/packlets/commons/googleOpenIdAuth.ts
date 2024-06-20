f0fce6673b971bf723fabb63a5396eaf6e6f26dc
Phumrapee Limpianchop <git@rayriffy.com>
2024-06-21 03:47:59 +0900

    chore: staged changes

--- src/packlets/commons/googleOpenIdAuth.ts
+++ src/packlets/commons/googleOpenIdAuth.ts
@@ -9,33 +9,38 @@ interface Options {
   allowedEmails: string[];
 }
 
-export const googleOpenIdAuth = ({ audiences, allowedEmails }: Options) =>
-  new Elysia({
-    name: "google-openid-auth",
-  })
-    .use(bearer())
-    .onBeforeHandle(async ({ bearer, set }) => {
-      const result = await jwtVerify<JWTPayload>(
-        bearer!,
-        createRemoteJWKSet(
-          new URL("https://www.googleapis.com/oauth2/v3/certs"),
-        ),
-        {
-          issuer: "https://accounts.google.com",
-          audience: audiences,
-        },
-      ).catch(() => null);
+export const googleOpenIdAuth = ({ audiences, allowedEmails }: Options) => {
+  return (app: Elysia) => {
+    const plugin = new Elysia({
+      name: "google-openid-auth",
+    })
+      .use(bearer())
+      .onBeforeHandle(async ({ bearer, set }) => {
+        const result = await jwtVerify<JWTPayload>(
+          bearer!,
+          createRemoteJWKSet(
+            new URL("https://www.googleapis.com/oauth2/v3/certs"),
+          ),
+          {
+            issuer: "https://accounts.google.com",
+            audience: audiences,
+          },
+        ).catch(() => null);
 
-      if (
-        process.env.NODE_ENV === "production" &&
-        (result === null ||
-          !result.payload.email_verified ||
-          !allowedEmails.includes(result.payload.email))
-      ) {
-        set.status = 400;
-        set.headers["WWW-Authenticate"] =
-          `Bearer realm='sign', error="invalid_request"`;
+        if (
+          process.env.NODE_ENV === "production" &&
+          (result === null ||
+            !result.payload.email_verified ||
+            !allowedEmails.includes(result.payload.email))
+        ) {
+          set.status = 400;
+          set.headers["WWW-Authenticate"] =
+            `Bearer realm='sign', error="invalid_request"`;
 
-        return "Unauthorized";
-      }
-    });
+          return "Unauthorized";
+        }
+      });
+
+    return app.use(plugin);
+  };
+};
