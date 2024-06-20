dcd5866558db510e2e839c41c41e41341d3936d0
Phumrapee Limpianchop <git@rayriffy.com>
2024-06-20 20:38:12 +0900

    refactor: auth

--- src/packlets/walletPasses/router.ts
+++ src/packlets/walletPasses/router.ts
@@ -1,5 +1,7 @@
 import { Elysia } from "elysia";
 
+import { bearer } from "@elysiajs/bearer";
+
 import { PKPass } from "passkit-generator";
 import sharp from "sharp";
 import dayjs from "dayjs";
@@ -16,122 +18,136 @@ const parseDate = (date: Date) => dayjs(date).utcOffset(7);
 
 export const router = new Elysia({
   prefix: "/walletPasses",
-}).get(
-  "/garten",
-  async ({ query, set }) => {
-    const { event, user } = query;
+})
+  .use(bearer())
+  .get(
+    "/garten",
+    async ({ query, set }) => {
+      const { event, user } = query;
 
-    // download an image
-    const image = await fetch(event.image).then((o) => o.arrayBuffer());
+      // download an image
+      const image = await fetch(event.image).then((o) => o.arrayBuffer());
 
-    // process image into 2 sizes
-    const sharpenImage = sharp(image).png({
-      compressionLevel: 7,
-    });
+      // process image into 2 sizes
+      const sharpenImage = sharp(image).png({
+        compressionLevel: 7,
+      });
 
-    const [thumbnail, thumbnail2x, wwdr, signerCert, signerKey] =
-      await Promise.all([
-        sharpenImage.resize(150, 150).toBuffer(),
-        sharpenImage.resize(300, 300).toBuffer(),
-        certs("wwdr"),
-        certs("garten/cert"),
-        certs("garten/key"),
-      ]);
-
-    const parsedDate = parseDate(event.date);
-    const parsedEndDate = event.endDate ? parseDate(event.endDate) : null;
-
-    // building the pass
-    const pass = new PKPass(
-      Object.fromEntries(
+      const [thumbnail, thumbnail2x, wwdr, signerCert, signerKey] =
         await Promise.all([
-          asset("background.png"),
-          asset("background@2x.png"),
-          asset("icon.png"),
-          asset("icon@2x.png"),
-          asset("logo.png"),
-          asset("logo@2x.png"),
-          asset("secondaryLogo.png"),
-          asset("secondaryLogo@2x.png"),
-          ["thumbnail.png", thumbnail],
-          ["thumbnail@2x.png", thumbnail2x],
-        ]),
-      ),
-      {
-        wwdr,
-        signerCert,
-        signerKey,
-        signerKeyPassphrase: PASS_GARDEN_SIGNER_KEY,
-      },
-      {
-        formatVersion: 1,
-        passTypeIdentifier: "pass.org.creatorsgarten.events",
-        teamIdentifier: APPLE_TEAM_ID,
-        organizationName: "Creatorsgarten",
-        description: "Creatorsgarten Event Ticket",
-        serialNumber: `${event.id}-${user.ticket.ref}`,
-        foregroundColor: "rgb(255, 255, 255)",
-        backgroundColor: "rgb(0, 168, 253)",
-        labelColor: "rgb(255, 255, 255)",
-        semantics: {
-          eventName: event.name,
-          eventType: "PKEventTypeConference",
-          eventStartDate: parsedDate.startOf("day").toISOString(),
-          eventEndDate: (parsedEndDate ?? parsedDate)
-            .endOf("day")
-            .toISOString(),
+          sharpenImage.resize(150, 150).toBuffer(),
+          sharpenImage.resize(300, 300).toBuffer(),
+          certs("wwdr"),
+          certs("garten/cert"),
+          certs("garten/key"),
+        ]);
+
+      const parsedDate = parseDate(event.date);
+      const parsedEndDate = event.endDate ? parseDate(event.endDate) : null;
+
+      // building the pass
+      const pass = new PKPass(
+        Object.fromEntries(
+          await Promise.all([
+            asset("background.png"),
+            asset("background@2x.png"),
+            asset("icon.png"),
+            asset("icon@2x.png"),
+            asset("logo.png"),
+            asset("logo@2x.png"),
+            asset("secondaryLogo.png"),
+            asset("secondaryLogo@2x.png"),
+            ["thumbnail.png", thumbnail],
+            ["thumbnail@2x.png", thumbnail2x],
+          ]),
+        ),
+        {
+          wwdr,
+          signerCert,
+          signerKey,
+          signerKeyPassphrase: PASS_GARDEN_SIGNER_KEY,
         },
+        {
+          formatVersion: 1,
+          passTypeIdentifier: "pass.org.creatorsgarten.events",
+          teamIdentifier: APPLE_TEAM_ID,
+          organizationName: "Creatorsgarten",
+          description: "Creatorsgarten Event Ticket",
+          serialNumber: `${event.id}-${user.ticket.ref}`,
+          foregroundColor: "rgb(255, 255, 255)",
+          backgroundColor: "rgb(0, 168, 253)",
+          labelColor: "rgb(255, 255, 255)",
+          semantics: {
+            eventName: event.name,
+            eventType: "PKEventTypeConference",
+            eventStartDate: parsedDate.startOf("day").toISOString(),
+            eventEndDate: (parsedEndDate ?? parsedDate)
+              .endOf("day")
+              .toISOString(),
+          },
+        },
+      );
+
+      pass.setRelevantDate(parsedDate.startOf("day").toDate());
+
+      pass.setBarcodes({
+        format: "PKBarcodeFormatQR",
+        message: user.ticket.ref,
+        altText: user.ticket.ref,
+      });
+
+      pass.type = "eventTicket";
+      pass.headerFields.push({
+        key: "date",
+        label: "DATE",
+        value: `${parsedDate.format("DD MMM")}${parsedEndDate ? ` - ${parsedEndDate.format("DD MMM")}` : ""}`,
+      });
+      pass.primaryFields.push({
+        key: "event",
+        label: "EVENT",
+        value: event.name,
+      });
+      pass.secondaryFields.push({
+        key: "loc",
+        label: "LOCATION",
+        value: event.location,
+      });
+      pass.auxiliaryFields.push(
+        {
+          key: "name",
+          label: "NAME",
+          value: user.name,
+        },
+        {
+          key: "ticket",
+          label: "TICKET",
+          value: user.ticket.type,
+        },
+      );
+      pass.backFields.push({
+        key: "notes",
+        value: "Beta",
+      });
+
+      set.headers["Content-Type"] = pass.mimeType;
+      set.headers["Content-disposition"] =
+        `attachment; filename=${event.id}.pkpass`;
+
+      return pass.getAsStream();
+    },
+    {
+      query: gartenPassModel,
+      beforeHandle: ({ bearer, set }) => {
+        if (
+          process.env.NODE_ENV === "production" &&
+          bearer !== process.env.SECRETS
+        ) {
+          set.status = 400;
+          set.headers["WWW-Authenticate"] =
+            `Bearer realm='sign', error="invalid_request"`;
+
+          return "Unauthorized";
+        }
       },
-    );
-
-    pass.setRelevantDate(parsedDate.startOf("day").toDate());
-
-    pass.setBarcodes({
-      format: "PKBarcodeFormatQR",
-      message: user.ticket.ref,
-      altText: user.ticket.ref,
-    });
-
-    pass.type = "eventTicket";
-    pass.headerFields.push({
-      key: "date",
-      label: "DATE",
-      value: `${parsedDate.format("DD MMM")}${parsedEndDate ? ` - ${parsedEndDate.format("DD MMM")}` : ""}`,
-    });
-    pass.primaryFields.push({
-      key: "event",
-      label: "EVENT",
-      value: event.name,
-    });
-    pass.secondaryFields.push({
-      key: "loc",
-      label: "LOCATION",
-      value: event.location,
-    });
-    pass.auxiliaryFields.push(
-      {
-        key: "name",
-        label: "NAME",
-        value: user.name,
-      },
-      {
-        key: "ticket",
-        label: "TICKET",
-        value: user.ticket.type,
-      },
-    );
-    pass.backFields.push({
-      key: "notes",
-      value: "Beta",
-    });
-
-    set.headers["Content-Type"] = pass.mimeType;
-    set.headers["Content-disposition"] =
-      `attachment; filename=${event.id}.pkpass`;
-
-    return pass.getAsStream();
-  },
-  {
-    query: gartenPassModel,
-  },
-);
+    },
+  );
